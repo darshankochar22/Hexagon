de2ec7e4eb796a032c40c02209e079df613919a8
darshankochar22 <darshannn022@gmail.com>
2025-10-05 21:25:32 +0530

    __

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <main className="page-wrap">
-      <h2>About</h2>
+      <h2>About us</h2>
       <p>Coming soon.</p>
     </main>
   )
